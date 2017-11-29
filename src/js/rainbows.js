c5f0ca334b3cc44a33190a8b9a627c3f77c49c65
Michael Lee <mleenavy@gmail.com>
2017-11-29 12:26:07 -0500

    sixified

--- src/js/rainbows.js
+++ src/js/rainbows.js
@@ -1,99 +1,95 @@
 console.log('This is rainbows.js')
 
-let appDiv = document.getElementById('app');
+let appDiv = document.getElementById('app')
 
-let myDiv = document.createElement('div');
-myDiv.className = "header";
-myDiv.setAttribute('id', 'myDiv');
+let myDiv = document.createElement('div')
+myDiv.className = "header"
+myDiv.setAttribute('id', 'myDiv')
 
-let myDivH2 = document.createElement('h2');
-myDivH2.style.margin = "5px";
-myDivH2.innerHTML = "My To Do List";
+let myDivH2 = document.createElement('h2')
+myDivH2.style.margin = "5px"
+myDivH2.innerHTML = "My To Do List"
 
-let myDivInput = document.createElement('input');
-myDivInput.setAttribute('type', 'text');
-myDivInput.setAttribute('id', 'myInput');
-myDivInput.setAttribute('placeholder', 'Title...');
+let myDivInput = document.createElement('input')
+myDivInput.setAttribute('type', 'text')
+myDivInput.setAttribute('id', 'myInput')
+myDivInput.setAttribute('placeholder', 'Title...')
 
-let myDivSpan = document.createElement('span');
-myDivSpan.className = "addBtn";
-myDivSpan.addEventListener('click', newElement);
-myDivSpan.innerHTML = "Add";
-
-
-myDiv.appendChild(myDivH2);
-myDiv.appendChild(myDivInput);
-myDiv.appendChild(myDivSpan);
+let myDivSpan = document.createElement('span')
+myDivSpan.className = "addBtn"
+myDivSpan.addEventListener('click', newElement)
+myDivSpan.innerHTML = "Add"
 
 
-let myUL = document.createElement('ul');
-myUL.setAttribute('id', 'myUL');
+myDiv.appendChild(myDivH2)
+myDiv.appendChild(myDivInput)
+myDiv.appendChild(myDivSpan)
 
-appDiv.appendChild(myDiv);
-appDiv.appendChild(myUL);
+
+let myUL = document.createElement('ul')
+myUL.setAttribute('id', 'myUL')
+
+appDiv.appendChild(myDiv)
+appDiv.appendChild(myUL)
 
 // ===========================================
 
 
-
 document.getElementById("myUL").innerHTML = localStorage.getItem("storedList")
-  
-
 
   // Create a "close" button and append it to each list item
-  var myNodelist = document.getElementsByTagName("LI");
-  var i;
-  for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+  let myNodelist = document.getElementsByTagName("LI")
+  for (let element of myNodelist) {
+    let span = document.createElement("SPAN")
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(txt)
+    element.appendChild(span)
   }
 
   // Click on a close button to hide the current list item
-  var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+  let close = document.getElementsByClassName("close")
+  for (let element of close) {
+    element.onclick = function() {
+      let div = this.parentElement
+      div.style.display = "none"
       localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
     }
   }
 
   // Add a "checked" symbol when clicking on a list item
-  var list = document.querySelector('ul');
+  let list = document.querySelector('ul')
   list.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
+      ev.target.classList.toggle('checked')
       localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
     }
-  }, false);
+  }, false)
 
   // Create a new list item when clicking on the "Add" button
   function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    let li = document.createElement("li")
+    let inputValue = document.getElementById("myInput").value
+    let t = document.createTextNode(inputValue)
+    
+    li.appendChild(t)
     if (inputValue === '') {
-      alert("You must write something!");
+      alert("You must write something!")
     } else {
-      document.getElementById("myUL").appendChild(li);
+      document.getElementById("myUL").appendChild(li)
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById("myInput").value = ""
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    let span = document.createElement("SPAN")
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(txt)
+    li.appendChild(span)
 
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
+    for (let element of close) {
+      element.onclick = function() {
+        let div = this.parentElement
+        div.style.display = "none"
       }
     }
     localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
