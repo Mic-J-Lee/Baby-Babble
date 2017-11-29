63cbe05509aff86deea147b5b8dcff940513e001
Michael Lee <mleenavy@gmail.com>
2017-11-29 16:27:48 -0500

    refactored rainbows

--- src/js/rainbows.js
+++ src/js/rainbows.js
@@ -34,10 +34,16 @@ appDiv.appendChild(myUL)
 
 // ===========================================
 
-
+// Retrieve and display list
 document.getElementById("myUL").innerHTML = localStorage.getItem("storedList")
 
-  // Create a "close" button and append it to each list item
+// Define list storing function
+var storeList = () => {
+  localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
+}
+
+// Create a function that appends a "close" button to a list item
+var addCloseButton = () => {
   let myNodelist = document.getElementsByTagName("LI")
   for (let element of myNodelist) {
     let span = document.createElement("SPAN")
@@ -46,51 +52,45 @@ document.getElementById("myUL").innerHTML = localStorage.getItem("storedList")
     span.appendChild(txt)
     element.appendChild(span)
   }
+}
 
-  // Click on a close button to hide the current list item
+// Create a function that removes a list item when its close button is clicked
+var clickToClose = () => {
   let close = document.getElementsByClassName("close")
   for (let element of close) {
     element.onclick = function() {
       let div = this.parentElement
-      div.style.display = "none"
-      localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
+      div.remove()
+      storeList()
     }
   }
+}
+clickToClose()
 
-  // Add a "checked" symbol when clicking on a list item
-  let list = document.querySelector('ul')
-  list.addEventListener('click', (ev) => {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked')
-      localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
-    }
-  }, false)
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul')
+list.addEventListener('click', (ev) => {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked')
+    storeList()
+  }
+}, false)
 
-  // Create a new list item when clicking on the "Add" button
-  function newElement() {
-    let li = document.createElement("li")
-    let inputValue = document.getElementById("myInput").value
-    let t = document.createTextNode(inputValue)
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  let li = document.createElement("li")
+  let inputValue = document.getElementById("myInput").value
+  let t = document.createTextNode(inputValue)
 
+  if (inputValue === '') {
+    alert("You must write something!")
+  } else {
     li.appendChild(t)
-    if (inputValue === '') {
-      alert("You must write something!")
-    } else {
-      document.getElementById("myUL").appendChild(li)
-    }
-    document.getElementById("myInput").value = ""
-
-    let span = document.createElement("SPAN")
-    let txt = document.createTextNode("\u00D7")
-    span.className = "close"
-    span.appendChild(txt)
-    li.appendChild(span)
-
-    for (let element of close) {
-      element.onclick = function() {
-        let div = this.parentElement
-        div.style.display = "none"
-      }
-    }
-    localStorage.setItem("storedList", document.getElementById("myUL").innerHTML)
+    document.getElementById("myUL").appendChild(li)
   }
+  document.getElementById("myInput").value = ""
+
+  addCloseButton()
+  clickToClose()
+  storeList()
+}
--- src/js/unicorns.js
+++ src/js/unicorns.js
@@ -1,1 +1,2 @@
-
+let storedList = localStorage.getItem("storedList")
+console.log('This is your To-Do list, stored as "storedList":' + storedList)
